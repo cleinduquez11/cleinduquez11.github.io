ae01b0af11db29da6f64b4ebe0cfdb0a26a79d85
Clein Winslee Duquez <103979569+cleinduquez11@users.noreply.github.com>
2023-11-17 16:32:16 +0800

    updated 11-17-23

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "09aebe418ecf2bbacd28c1db19e6ed5c
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "609183ac4adcb3beceff11c9565f4ddb",
 "assets/lib/models/Assets/music/steal-the-show.mp3": "22bbf1c80b44bba094ce8da074ce9364",
-"assets/NOTICES": "b6a7b33eedaeb36ef6691fa93e9e9836",
+"assets/NOTICES": "59770ee649f9bdc2ee103e525fbb5348",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/packages/getwidget/icons/dribble.png": "1e36936e4411f32b0e28fd8335495647",
 "assets/packages/getwidget/icons/facebook.png": "293dc099a89c74ae34a028b1ecd2c1f0",
@@ -35,9 +35,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "09aebe418ecf2bbacd28c1db19e6ed5c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7b2c65843364b54879a04539a2c88a5a",
-"/": "7b2c65843364b54879a04539a2c88a5a",
-"main.dart.js": "6c6bcf68401e4b999194b01e66dfd1a7",
+"index.html": "a833f21857734c8d20045e520d44ecc9",
+"/": "a833f21857734c8d20045e520d44ecc9",
+"main.dart.js": "7a2bb26652a997f100f494261b17c7b1",
 "manifest.json": "b744b2e705afa7a664ac7509b70d78bb",
 "version.json": "aecaf7fd218382e439978b69561c66bc"};
 // The application shell files that are downloaded before a service worker can
