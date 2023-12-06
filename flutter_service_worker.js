d85775360605665854af1a42e228b69159a943f1
Clein Winslee Duquez <103979569+cleinduquez11@users.noreply.github.com>
2023-12-06 09:36:31 +0800

    updated 12-6-23

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,8 +40,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "09aebe418ecf2bbacd28c1db19e6ed5c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d1c57318b91c6ab891c9630d0427ee4e",
-"/": "d1c57318b91c6ab891c9630d0427ee4e",
+"index.html": "45a61b6566be924cd1b159626f737ec3",
+"/": "45a61b6566be924cd1b159626f737ec3",
 "main.dart.js": "c480aa256ade68fee861d1e520aca22e",
 "manifest.json": "4ae76d628baf55f64db591537de15983",
 "version.json": "aecaf7fd218382e439978b69561c66bc"};
